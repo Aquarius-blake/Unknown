424896f05cf7c60116934b8d89b3374146d830cf
Aquarius-blake <danbaid72@gmail.com>
2022-03-30 20:39:39 +0000

    Minor

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class Ninjac extends StatelessWidget {
             trew= "Access Denied";
 
           },
-          child: Text("Find"),
+          child: Text("Locate"),
         ),
       appBar: AppBar(
 
